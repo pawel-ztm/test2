5c9790e63fe88e493de8e4908228033da3f8b8fe
pawel-ztm <114384018+pawel-ztm@users.noreply.github.com>
2022-12-13 14:44:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Naucz się React JS!:P
+          Naucz się React JS!
         </a>
       </header>
     </div>
