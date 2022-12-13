08928a958357fadb924dff19a7de3da9159d083e
pawel-ztm <114384018+pawel-ztm@users.noreply.github.com>
2022-12-13 14:18:03 +0100

    Update paragraf.js

--- src/paragraf.js
+++ src/paragraf.js
@@ -6,6 +6,7 @@ function paragraf() {
             <p>Zmiana tekstu przez kogoś innego</p>
             <p>Inna osoba dodała swoje trzy groszę!</p>
             <p>To jeszcze nie koniec, a ja też mam coś do powiedzienia</p>
+            <p>Jeszcze jedna poprawka</p>
         </div>
     )
 }
