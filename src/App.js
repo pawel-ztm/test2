16daaeb291966958db5bb8a4da11b59c1bee0b98
pawel-ztm <114384018+pawel-ztm@users.noreply.github.com>
2022-12-19 12:19:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Przykładowy tekst
+         Zmiana tekstu na stronie
         </a>
       </header>
     </div>
