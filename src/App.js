9349038e5a150b8126155026e2c2b504cf2b83bb
pawel-ztm <114384018+pawel-ztm@users.noreply.github.com>
2022-12-19 12:31:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Ważna informacja!
+         Przykładowa informacja!
         </a>
       </header>
     </div>
