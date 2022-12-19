6e6de7fe3ee5e00add2d8cfefd6ba0ff570254fc
pawel-ztm <114384018+pawel-ztm@users.noreply.github.com>
2022-12-19 08:32:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Pusty projekt!
+          Pusty projekt
         </a>
       </header>
     </div>
